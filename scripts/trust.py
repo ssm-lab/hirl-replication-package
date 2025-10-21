d960ac3b1e33459ae55c2fd5d4a8db60d2f4bc7e
Baran Shajari <148021398+Baranshajari@users.noreply.github.com>
2025-10-21 14:20:53 -0400

    Remove unused column variable from trust.py

--- scripts/trust.py
+++ scripts/trust.py
@@ -15,10 +15,7 @@ from matplotlib.ticker import MaxNLocator
 inputFolder = './data'
 outputFolder = './output'
 data = pd.read_excel(f'{inputFolder}/data-extracted.xlsx')
-column = 'Sus. dimensions'
 
-columnFilename = {
-    'Sus. dimensions' : 'sustainability_dim'
 }
 def create_bar_chart_from_file(file_path, x_column, y_column, title, x_label, y_label, output_filename="trust.pdf"):
     """
