5441a244e9ebe4097c255ac4b34e54e23ac7ddbe
Baran Shajari <148021398+Baranshajari@users.noreply.github.com>
2025-10-21 14:05:10 -0400

    Add imports and initial setup in upset.py

--- scripts/upset.py
+++ scripts/upset.py
@@ -1,1 +1,18 @@
+from upsetplot import from_memberships
+from upsetplot import plot
+from matplotlib import pyplot
+import matplotlib
+from itertools import combinations
 
+import numpy as np
+import os
+import pandas as pd
+from collections import Counter
+from matplotlib import pyplot as plt
+from matplotlib.offsetbox import AnchoredText
+from matplotlib.ticker import MaxNLocator
+
+inputFolder = './data'
+outputFolder = './output'
+data = pd.read_excel(f'{inputFolder}/data-extracted.xlsx')
+column = 'Sus. dimensions'
