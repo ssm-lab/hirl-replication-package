acb144c9314265125e5788f5c331608a46949f2f
Istvan David <david-istvan@users.noreply.github.com>
2025-10-30 16:06:19 -0400

    Update phase1.py

--- scripts/phase1.py
+++ scripts/phase1.py
@@ -11,17 +11,16 @@ from collections import Counter
 from matplotlib import pyplot as plt
 from matplotlib.offsetbox import AnchoredText
 from matplotlib.ticker import MaxNLocator
-inputFolder = './phase1-G1-18'
-outputFolder = './phase1.pdf'
-data = pd.read_excel(f'{inputFolder}/phase1-G1-18.xlsx')
+import plot_likert
+
+inputFolder = './data'
+outputFolder = './output'
+
+data = pd.read_excel(f'{inputFolder}/P1-1-30.xlsx')
 data_df.index = pd.to_datetime(data_df.index) #convert index column into DateTime column data_df.plot()
 into DateTime column data_df.plot()
-
 
 data = pd.DataFrame({'Q1': {0: 'Strongly disagree', 1: 'Agree', ...},
                      'Q2': {0: 'Disagree', 1: 'Strongly agree', ...}})
 
-# Now plot it!
-import plot_likert
-
-plot_likert.plot_likert(phase1-G1-18, plot_likert.scales.agree, plot_percentage=True);
+plot_likert.plot_likert(data, plot_likert.scales.agree, plot_percentage=True);
